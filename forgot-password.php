893d1ad0fb25b8fe5f6e23dbabbe2d9c317bd0ae
Aidan Woods <aidantwoods@gmail.com>
2016-07-17 17:59:32 +0100

    added page structure for forgot password

---
+++ forgot-password.php
@@ -0,0 +1,56 @@
+<?php
+
+include("html.php");
+
+?>
+
+
+<html>
+<head>
+	<script type="text/javascript" src="/javascript.js"></script>
+	<link rel="stylesheet" type="text/css" href="/main.css"></link>
+</head>
+
+<body>
+	<div class="wrapper">
+		<div class="container">
+			<?php header_code(); ?>
+			<div class="register-box">
+				<h3>Reset Password</h3>
+				<?php
+					if(isset($_GET['req'])){ ?>
+					<ol class="error">
+					<?php
+						$required = explode(";", $_GET['req']);
+						?>
+						The following are required:
+						<?php
+						$z="";
+						foreach($required as $item){
+						?><li><?php echo htmlentities($z.$item); ?></li><?php
+						$z=", ";
+						}
+						echo "."
+						?>
+					</ol>
+					<?php
+					}
+					if(isset($_GET['err'])){ ?>
+					<?php
+						$error = explode(";", $_GET['err']);
+						foreach($error as $item){
+						?><ol class="error"><?php echo htmlentities($item); ?></ol><?php
+						}
+						?>
+					<?php
+					}
+				 ?>
+				<form method="POST" action="forgot_password.php<?php if(isset($_GET['redirect'])){echo '?redirect=' . $_GET['redirect'];} ?>">
+					<input type="text" name="email" placeholder="Email" />
+					<input type="submit" name="submit" value="Reset" />
+				</form>
+			</div>	
+		</div>
+	</div>
+</body>
+</html>
