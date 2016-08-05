ec2853e83bee6808753e51502a34973c3f160cbb
Aidan Woods <aidan.woods@sainsburys.co.uk>
2016-08-05 14:30:34 +0100

    modified product page

--- product.php
+++ product.php
@@ -1,6 +1,7 @@
 <?php
 
 include("includes.php");
+include("connect-user.php");
 
 ?>
 
@@ -28,8 +29,7 @@ include("includes.php");
 				<div class="row">Quantity <?php echo $result['quantity']; ?></div>		
 			</div>
 			<?php
-				$sth = $dbh->prepare('SELECT * FROM comments WHERE product_id=:id');
-				$sth->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+				$sth = $dbh->prepare('SELECT user_id, comment FROM comments WHERE product_id='.$_GET['id']);
 				$sth->execute();
 				$comments = $sth->fetchAll();
 				$n = $sth->rowCount();
@@ -55,14 +55,14 @@ include("includes.php");
 					<?php
 					foreach($comments as $comment){
 						$sth = $dbh->prepare('SELECT * FROM users WHERE id=:id');
-						$sth->bindParam(':id', $comment['user_id'], PDO::PARAM_INT);
+						$sth->bindParam(':id', $comment[0], PDO::PARAM_INT);
 						$sth->execute();
 						$results = $sth->fetchAll();
 						echo '
 						<div class="comment-row">
 							<h4>' . htmlentities($results[0]['first_name']) . '</h4>
 							<div class="comment-body">
-								' . htmlentities($comment['comment']) . '
+								' . htmlentities($comment[1]) . '
 							</div>
 						</div>';
 					}
